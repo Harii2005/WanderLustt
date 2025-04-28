b25ac590f90174239c2df7199fb7fd7ca5f17195
Harii2005 <harikrishnankrkv@gmail.com>
2025-04-28 23:31:35 +0530

    new create and show route done..

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path')
 
 app.set('view engine' , 'ejs'); 
 app.set('views' , path.join(__dirname , 'views'));
+app.use(express.urlencoded({extended : true}));
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -51,4 +52,29 @@ app.get('/listings' , async (req , res) =>{
     res.render('listings/index.ejs' , {allListings});
 });
 
+
+//new route
+app.get('/listings/new' , (req , res) => {
+    res.render('listings/new.ejs');
+});
+
+//use new route before shoe route bez if  /listings/:id accesing 1st then app.js will consider the new also as a id in  /listings/new
+
+//show route
+app.get('/listings/:id' , async (req , res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listings/show.ejs' , {listing});
+});
+
+
+//create route
+app.post('/listings' , async(req , res) => {
+    // let {title , discription , image , price , country  , location} = req.body;
+    let listing = req.body.listing;
+    const newlisting = new Listing(listing);
+    console.log(listing);
+    await newlisting.save();
+    res.redirect('/listings');
+});
 
