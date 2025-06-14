80071ded93b22d3bd8ee9e59064687973d2397f8
Harii2005 <harikrishnankrkv@gmail.com>
2025-06-14 23:50:03 +0530

    added listing preview in edit form in lower quality

--- Controller/listings.js
+++ Controller/listings.js
@@ -26,17 +26,6 @@ module.exports.showListing = (async  (req , res) => {
 });
 
 
-// module.exports.createListing = async (req, res, next) => {
-//     // console.log("req.body :",req.body);
-//     // console.log("Listing data:", req.body.listing) 
- 
-//     const newListing = new Listing(req.body.listing);
-//     newListing.owner = req.user._id;//req.user will have the information of the user which is logged in ..
-//     await newListing.save();
-
-//     req.flash("success" , "new listing created");
-//     res.redirect('/listings');
-// }
 module.exports.createListing = async (req, res, next) => {
     // console.log("req.body :",req.body);
     // console.log("Listing data:", req.body.listing) 
@@ -60,16 +49,19 @@ module.exports.editListing = async (req ,res) => {
         req.flash("error" , "the listing you requested does not exists sorry!!");
         res.redirect('/listings');
     }
+
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload" , "/upload/h_300,w_250");//to reduce the quality of the image for smooth performance
     req.flash("success" , "listing edited");
-    res.render('listings/edit.ejs' , {listing});
+    res.render('listings/edit.ejs' , {listing , originalImageUrl});
 }
 
 module.exports.updateListing = async (req , res) => {
     let {id} = req.params;
     let listing = await Listing.findByIdAndUpdate(id , {...req.body.listing});
-    
+
     // If a new file is uploaded, update the image
-    if (req.file) {
+    if (typeof req.file !== "undefined") {
         let url = req.file.path;
         let filename = req.file.filename;
         listing.image = {url, filename};
