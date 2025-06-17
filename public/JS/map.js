6e56549b6d0bf1c4c9965a1b5f8142a644263640
Harii2005 <harikrishnankrkv@gmail.com>
2025-06-17 17:44:20 +0530

    implemented maps

--- public/JS/map.js
+++ public/JS/map.js
@@ -1,14 +1,17 @@
 
 mapboxgl.accessToken = mapToken ;
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [77.2090, 28.6139],//here fist longitute and then latitude
+    center: coordinates,//here fist longitute and then latitude
     // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
 
 
+// console.log(coordinates);
+
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker()
-    .setLngLat([12.554729, 55.70651])
+    .setLngLat(coordinates)
     .addTo(map);
